50cfe7e9dbd56c1f03be73b83626aa411ef645b7
realioseb <soso.bichinashvili@gmail.com>
2013-12-15 18:42:27 +0400

    Daviwye!
    daviwye proeqtis keteba, gavakete dziritadi html da css, monaxazebi,
    dizaini... exla nelnela avamushaveb saits

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+  <head>
+    <title>Address Book</title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+  </head>
+  <body>
+    
+    <div class="container">
+    <h1>My address book</h1>
+    <br>
+        <div class="left_list">
+            <div class="list_header">
+                Contacts:
+            </div>
+            <ul class="contacts">
+                <li>soso</li>
+                <li>gio</li>
+                <li>teo</li>
+            </ul>
+            <form class="search_form">
+                <input type="search_address" id="search_address" placeholder="Type contact name">
+            </form>
+        </div>
+
+        <div class="right_content">
+            <div class="right_header">
+                <h3>Contact Info:</h3>
+                <div class="add_button">
+                    Add
+                </div>
+                <div style="clear: both"></div>
+            </div>
+        </div>
+        <div style="clear: both;"></div>
+    </div>
+    <script src="https://code.jquery.com/jquery.js"></script>
+  </body>
 </html>
