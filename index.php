e9180bf2084282dee872725e821f475611734f29
realioseb <soso.bichinashvili@gmail.com>
2013-12-17 21:47:30 +0400

    droa saqmes mivxedo :S
    mokled, serchis formac gavakete vizualurad da axla droa rom realur
    saqmes shevudge

--- index.php
+++ index.php
@@ -10,25 +10,27 @@
         <h1>My address book</h1>
         <br>
     
-        <div class="header border" id="left">
+        <div class="MainStyle border" id="left">
             Contacts:
         </div>
-        <div class="header border" id="right">
+        <div class="MainStyle border" id="right">
                 Contact Info:
         </div>
-        <div class="header border button" id="add_button" title="Create new contact">
+        <div class="MainStyle border button" id="add_button" title="Create new contact">
                     ADD
         </div>
         <div style="clear: both;"></div>
         
         <div class="content border" id="list">
-            <form class="search_form">
-                <input type="search_address" id="search_address" placeholder="Type contact name">
-            </form>
         </div>
+        
         <div class="content border" id="info">
-            
         </div>
+        
+        <form class="search_form border">
+            <input type="search_address" id="search_address" placeholder="Type contact name">
+        </form>
+        
         <div style="clear: both;"></div>
     </div>
     <script src="https://code.jquery.com/jquery.js"></script>
